b7685a5ca8289b051ff8d2342ac3db2e9e57d52e
youwol-guillaume <greinisch@youwol.com>
2023-03-09 11:55:03 +0100

    🔧 sync config. files w/ pipeline TS

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -1,19 +1,70 @@
 
 const runTimeDependencies = {
-    "externals": {},
+    "externals": {
+        "rxjs": "^6.5.5",
+        "@youwol/flux-view": "^1.1.0",
+        "@youwol/fv-tree": "^0.2.3",
+        "@youwol/cdn-client": "^1.0.10"
+    },
     "includedInBundle": {}
 }
-const externals = {}
-const exportedSymbols = {}
+const externals = {
+    "rxjs": {
+        "commonjs": "rxjs",
+        "commonjs2": "rxjs",
+        "root": "rxjs_APIv6"
+    },
+    "@youwol/flux-view": {
+        "commonjs": "@youwol/flux-view",
+        "commonjs2": "@youwol/flux-view",
+        "root": "@youwol/flux-view_APIv1"
+    },
+    "@youwol/fv-tree": {
+        "commonjs": "@youwol/fv-tree",
+        "commonjs2": "@youwol/fv-tree",
+        "root": "@youwol/fv-tree_APIv02"
+    },
+    "@youwol/cdn-client": {
+        "commonjs": "@youwol/cdn-client",
+        "commonjs2": "@youwol/cdn-client",
+        "root": "@youwol/cdn-client_APIv1"
+    }
+}
+const exportedSymbols = {
+    "rxjs": {
+        "apiKey": "6",
+        "exportedSymbol": "rxjs"
+    },
+    "@youwol/flux-view": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/flux-view"
+    },
+    "@youwol/fv-tree": {
+        "apiKey": "02",
+        "exportedSymbol": "@youwol/fv-tree"
+    },
+    "@youwol/cdn-client": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/cdn-client"
+    }
+}
 
-// eslint-disable-next-line @typescript-eslint/ban-types -- allow to allow no secondary entries
-const mainEntry : Object = {
+const mainEntry : {entryFile: string,loadDependencies:string[]} = {
     "entryFile": "./lib/index.ts",
     "loadDependencies": []
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-types -- allow to allow no secondary entries
-const secondaryEntries : Object = {}
+const secondaryEntries : {[k:string]:{entryFile: string, name: string, loadDependencies:string[]}}= {
+    "journal": {
+        "entryFile": "./lib/journal/index.ts",
+        "loadDependencies": [
+            "rxjs",
+            "@youwol/flux-view"
+        ],
+        "name": "journal"
+    }
+}
+
 const entries = {
      '@youwol/logging': './lib/index.ts',
     ...Object.values(secondaryEntries).reduce( (acc,e) => ({...acc, [`@youwol/logging/${e.name}`]:e.entryFile}), {})
@@ -21,7 +72,7 @@ const entries = {
 export const setup = {
     name:'@youwol/logging',
         assetId:'QHlvdXdvbC9sb2dnaW5n',
-    version:'0.1.0',
+    version:'0.1.1-wip',
     shortDescription:"Logging for Youwol front apps",
     developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/logging',
     npmPackage:'https://www.npmjs.com/package/@youwol/logging',
@@ -32,16 +83,20 @@ export const setup = {
     externals,
     exportedSymbols,
     entries,
+    secondaryEntries,
     getDependencySymbolExported: (module:string) => {
         return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
     },
 
-    installMainModule: ({cdnClient, installParameters}:{cdnClient, installParameters?}) => {
+    installMainModule: ({cdnClient, installParameters}:{
+        cdnClient:{install:(unknown) => Promise<Window>},
+        installParameters?
+    }) => {
         const parameters = installParameters || {}
         const scripts = parameters.scripts || []
         const modules = [
             ...(parameters.modules || []),
-            ...mainEntry['loadDependencies'].map( d => `${d}#${runTimeDependencies.externals[d]}`)
+            ...mainEntry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
         ]
         return cdnClient.install({
             ...parameters,
@@ -51,20 +106,24 @@ export const setup = {
             return window[`@youwol/logging_APIv01`]
         })
     },
-    installAuxiliaryModule: ({name, cdnClient, installParameters}:{name: string, cdnClient, installParameters?}) => {
+    installAuxiliaryModule: ({name, cdnClient, installParameters}:{
+        name: string,
+        cdnClient:{install:(unknown) => Promise<Window>},
+        installParameters?
+    }) => {
         const entry = secondaryEntries[name]
+        if(!entry){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
         const parameters = installParameters || {}
         const scripts = [
             ...(parameters.scripts || []),
-            `@youwol/logging#0.1.0~dist/@youwol/logging/${entry.name}.js`
+            `@youwol/logging#0.1.1-wip~dist/@youwol/logging/${entry.name}.js`
         ]
         const modules = [
             ...(parameters.modules || []),
             ...entry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
         ]
-        if(!entry){
-            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
-        }
         return cdnClient.install({
             ...parameters,
             modules,
@@ -72,5 +131,13 @@ export const setup = {
         }).then(() => {
             return window[`@youwol/logging/${entry.name}_APIv01`]
         })
+    },
+    getCdnDependencies(name?: string){
+        if(name && !secondaryEntries[name]){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const deps = name ? secondaryEntries[name].loadDependencies : mainEntry.loadDependencies
+
+        return deps.map( d => `${d}#${runTimeDependencies.externals[d]}`)
     }
 }
