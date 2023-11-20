acb7785f84f00b3db8510535d7f4c8a2bd068b23
youwol-guillaume <greinisch@youwol.com>
2023-11-20 10:06:05 +0100

    ⬆️ `template.py` => `rxjs#^7`, `rx-vdom#^1.0.1` and co.

--- template.py
+++ template.py
@@ -9,6 +9,11 @@ folder_path = Path(__file__).parent
 
 pkg_json = parse_json(folder_path / 'package.json')
 
+externals = {
+    'rxjs': '^7.5.6',
+    '@youwol/rx-vdom': '^1.0.1',
+    '@youwol/rx-tree-views': '^0.3.1',
+}
 
 template = Template(
     path=folder_path,
@@ -19,15 +24,11 @@ template = Template(
     author=pkg_json['author'],
     dependencies=Dependencies(
         runTime=RunTimeDeps(
-            externals={
-                'rxjs': '^6.5.5',
-                '@youwol/flux-view': '^1.1.0',
-                '@youwol/fv-tree': '^0.2.3',
-            }
+            externals=externals
         ),
         devTime={
             # Included for type definitions
-            "@youwol/cdn-client": "^2.0.3"
+            "@youwol/webpm-client": "^3.0.0"
         }
     ),
     bundles=Bundles(
@@ -39,7 +40,7 @@ template = Template(
             AuxiliaryModule(
                 name='journal',
                 entryFile="./lib/journal/index.ts",
-                loadDependencies=['rxjs', '@youwol/flux-view', '@youwol/fv-tree']
+                loadDependencies=list(externals.keys())
             )
         ]
     ),
@@ -52,8 +53,16 @@ shutil.copyfile(
     src=folder_path / '.template' / 'src' / 'auto-generated.ts',
     dst=folder_path / 'src' / 'auto-generated.ts'
 )
-for file in ['README.md', '.gitignore', '.npmignore', '.prettierignore', 'LICENSE', 'package.json',
-             'tsconfig.json', 'webpack.config.ts']:
+for file in [
+    'README.md',
+    '.gitignore',
+    '.npmignore',
+    '.prettierignore',
+    'LICENSE',
+    'package.json',
+    # 'tsconfig.json', need to reference rx-vdom-config.ts
+    'webpack.config.ts'
+]:
     shutil.copyfile(
         src=folder_path / '.template' / file,
         dst=folder_path / file
