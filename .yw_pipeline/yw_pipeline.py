5c59c0213836d17b96bdc02548ccd382c950417f
youwol-guillaume <greinisch@youwol.com>
2024-07-17 16:58:00 +0200

    👽️ Sync. `yw_pipeline`

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,6 +1,6 @@
-from youwol.app.environment import IPipelineFactory, YouwolEnvironment
-from youwol.app.environment.models_project import JsBundle, Link, Pipeline
-from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig
+from youwol.app.environment import YouwolEnvironment
+from youwol.app.routers.projects import IPipelineFactory, JsBundle, Link, Pipeline
+from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig, PublishConfig
 from youwol.utils.context import Context
 
 
